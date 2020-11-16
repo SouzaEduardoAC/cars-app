ad5ca4bc41f9b4ca75df85f8e327075514283796
Eduardo Augusto Coleta de Souza <souzaeduardoac@localhost.localdomain>
2020-11-16 11:50:01 -0300

    Added shared preferences lib

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:carsapp/utils/prefs.dart';
 import 'package:carsapp/widgets/car_listview.dart';
 import 'package:carsapp/widgets/drawer.dart';
 import 'package:flutter/material.dart';
@@ -8,29 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+
+    Future<int> future = Prefs.getInt("tabIdx");
+    future.then((int tabIdx) => _tabController.index = tabIdx);
+
+    _tabController.addListener(() {
+      Prefs.setInt("tabIdx", _tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Cars'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(text: 'Classic',),
-              Tab(text: 'Sportive',),
-              Tab(text: 'Luxury',),
-            ],
-          ),
-        ),
-        drawer: NavDrawer(),
-        body: TabBarView(
-          children: <Widget>[
-            _classicCars(),
-            _sportsCars(),
-            _luxuryCars(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cars'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(text: 'Classic',),
+            Tab(text: 'Sportive',),
+            Tab(text: 'Luxury',),
           ],
         ),
+      ),
+      drawer: NavDrawer(),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          _classicCars(),
+          _sportsCars(),
+          _luxuryCars(),
+        ],
       ),
     );
   }
